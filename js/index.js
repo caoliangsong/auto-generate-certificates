80f4d5fec4cbc80c5bf9f6e0128f3d19bbc1d477
caols <404508644@qq.com>
2021-11-19 11:52:53 +0800

    fix

--- js/index.js
+++ js/index.js
@@ -5,71 +5,82 @@
       username: "",
       datetime: "",
       hasCert: false,
-      img:'',
-      percent: 20
+      img: "",
+      percent: 20,
     },
-    created(){
+    created() {
       // 预加载模板图3M
-      const self = this
-      let image = new Image()
-      image.src = './img/demo.png'
-      image.onload = function(r){
-        self.percent = 100
-      }
+      const self = this;
+      let image = new Image();
+      image.src = "./img/demo.png";
+      image.onload = function (r) {
+        self.percent = 100;
+      };
     },
     methods: {
-
       onGenerateHandle() {
         if (!this.username || !this.datetime) {
           return alert("请输入姓名或日期");
         }
         this.img = document.getElementById("demo");
-        this.img2Canvas()
+        this.img2Canvas();
       },
       img2Canvas() {
-        this.hasCert = true
+        this.hasCert = true;
         let canvas = document.getElementById("myCanvas");
         canvas.width = 500;
         canvas.height = 750;
         let ctx = canvas.getContext("2d");
         ctx.drawImage(this.img, 0, 0, 500, 750);
-        this.watermark(canvas, 103, 168, 150, 400, false).then(res => {
-          console.log(res)
-        })
+        this.watermark(canvas, 103, 168, 150, 400, false).then((res) => {
+          console.log(res);
+        });
       },
-      onDownloadHandle(){
+      onDownloadHandle() {
+        if (!this.username || !this.datetime) {
+          return alert("请输入姓名或日期");
+        }
+        this.img = document.getElementById("demo");
         let canvas = document.createElement("canvas");
         // let canvas = document.getElementById("myCanvas2");
         canvas.width = this.img.width;
         canvas.height = this.img.height;
         let ctx = canvas.getContext("2d");
         ctx.drawImage(this.img, 0, 0);
-        this.watermark(canvas, 103, 168, 150, 400, true).then(res => {
-          console.log(res)
-          var a = document.createElement('a')
-          var url = window.URL.createObjectURL(res)
-          a.download = `${this.username}-明康汇结业证书.png`
-          a.href = url
-          a.click()
-          window.URL.revokeObjectURL(url)
-        })
+        this.watermark(canvas, 103, 168, 150, 400, true).then((res) => {
+          console.log(res);
+          var a = document.createElement("a");
+          var url = window.URL.createObjectURL(res);
+          a.download = `${this.username}-明康汇结业证书.png`;
+          a.href = url;
+          a.click();
+          window.URL.revokeObjectURL(url);
+        });
       },
 
-      watermark (canvas, x, y,x1,y1, isDownLoad = false) {
+      watermark(canvas, x, y, x1, y1, isDownLoad = false) {
         return new Promise((resolve, reject) => {
-          let ctx = canvas.getContext('2d')
-          ctx.font = isDownLoad ? `${12 * 7}px 楷体` : `12px 楷体`
-          ctx.fillStyle = "#545453"
-          ctx.textAlign = 'left'
-          ctx.fillText(this.datetime, isDownLoad ? x*7 :x, canvas.height - (isDownLoad ? y *7 : y))
+          let ctx = canvas.getContext("2d");
+          ctx.font = isDownLoad ? `${12 * 7}px 楷体` : `12px 楷体`;
+          ctx.fillStyle = "#545453";
+          ctx.textAlign = "left";
+          ctx.fillText(
+            this.datetime,
+            isDownLoad ? x * 7 : x,
+            canvas.height - (isDownLoad ? y * 7 : y)
+          );
 
-           ctx.font = isDownLoad ? `${60 * 7}px 楷体` : `60px 楷体`
-           ctx.fillStyle = "#545453"
-           ctx.textAlign = 'left'
-           ctx.fillText(this.username, isDownLoad ? x1*7 :x1, isDownLoad ? y1*7 :y1)
-          canvas.toBlob(blob => resolve(blob))
-        })
-      }
+          ctx.font = isDownLoad ? `${60 * 7}px 楷体` : `60px 楷体`;
+          ctx.fillStyle = "#545453";
+          ctx.textAlign = "left";
+          ctx.fillText(
+            this.username,
+            isDownLoad ? x1 * 7 : x1,
+            isDownLoad ? y1 * 7 : y1
+          );
+          canvas.toBlob((blob) => resolve(blob));
+        });
+      },
     },
   };
   window.app = new Vue(options);
