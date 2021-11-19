476d0a085876d594d3aa31106baa87450545d5a6
caols <404508644@qq.com>
2021-11-19 13:24:01 +0800

    增加导入excel

--- js/index.js
+++ js/index.js
@@ -16,13 +16,13 @@
       image.onload = function (r) {
         self.percent = 100;
       };
+      this.img = image;
     },
     methods: {
       onGenerateHandle() {
         if (!this.username || !this.datetime) {
           return alert("请输入姓名或日期");
         }
-        this.img = document.getElementById("demo");
         this.img2Canvas();
       },
       img2Canvas() {
@@ -40,7 +40,6 @@
         if (!this.username || !this.datetime) {
           return alert("请输入姓名或日期");
         }
-        this.img = document.getElementById("demo");
         let canvas = document.createElement("canvas");
         // let canvas = document.getElementById("myCanvas2");
         canvas.width = this.img.width;
@@ -80,6 +79,55 @@
           );
           canvas.toBlob((blob) => resolve(blob));
         });
+      },
+
+      onImportHandle(e) {
+        console.log(e.target.files);
+        const self = this;
+        const { files } = e.target;
+        if (files.length > 0) {
+          const f = files[0];
+          let reader = new FileReader();
+          reader.readAsBinaryString(f);
+          reader.onload = function (res) {
+            let data = res.target.result;
+            let workbook = XLSX.read(data, { type: "binary" });
+            var sheetNames = workbook.SheetNames; // 工作表名称集合
+            let allNames = [];
+            let datetime = "";
+            sheetNames.forEach((name) => {
+              var worksheet = workbook.Sheets[name]; // 只能通过工作表名称来获取指定工作表
+              console.log("worksheet", worksheet);
+              for (var key in worksheet) {
+                // .v是读取单元格的原始值,.t是单元格内容类型,.f是单元公式  !开头是有合并,c是列r是行(从0开始算)
+                // 读取日期
+                if (key == "C3") {
+                  const t = worksheet[key].w.split("/");
+                  datetime = `20${t[2]}-${t[0]}-${t[1]}`;
+                }
+
+                if (key.indexOf("B") > -1 && key >= "B3") {
+                  allNames.push(worksheet[key].v);
+                }
+                console.log(
+                  key,
+                  key[0] === "!" ? worksheet[key] : worksheet[key].v
+                );
+              }
+            });
+
+            self.datetime = datetime;
+            console.log(allNames);
+            for (let i = 0; i < allNames.length; i++) {
+              (function (j) {
+                setTimeout(() => {
+                  self.username = allNames[j];
+                  self.onDownloadHandle();
+                }, j * 2000);
+              })(i);
+            }
+          };
+        }
       },
     },
   };
